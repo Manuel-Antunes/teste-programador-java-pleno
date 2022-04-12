64d717d1edc06d0d421e89289d38fb6f939e2672
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-11 22:32:31 -0300

    feat: create navbar component to control the routes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <Navbar />
 </template>
 
 <style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img class="fluid" alt="Vue logo" src="../assets/logo.png" width="48px"/>
+            Zé's Company
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav ">
+                <a class="nav-link" href="#">Clients</a>
+                <a class="nav-link" href="#">Products</a>
+                <a class="nav-link" href="#">Orders</a>
+            </div>
+        </div>
+    </div>
+</nav>
+</template>
+
