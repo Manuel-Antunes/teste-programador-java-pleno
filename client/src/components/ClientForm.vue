1daf47958599cce7ba3e49c16991fc65f01d026d
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-12 03:48:45 -0300

    feat: create product and client form

---
+++ client/src/components/ClientForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-lg">
+      <div class="row justify-content-center my-5">
+        <div class="col-lg-6">
+          <form>
+            <label for="name" class="form-label"></label>
+            <div class="input-group">
+              <input type="text" id="name" class="form-control" placeholder="Nome" />
+            </div>
+            <label for="cpf" class="form-label"></label>
+            <div class="input-group">
+              <input type="text" id="cpf" class="form-control" placeholder="CPF"/>
+            </div>
+            <label for="phone" class="form-label"></label>
+            <div class="input-group">
+              <input type="tel" id="phone" class="form-control" placeholder="Telefone" />
+            </div>
+            <label for="email" class="form-label"></label>
+            <div class="input-group">
+              <input type="text" id="email" class="form-control" placeholder="Email" />
+            </div>
+            <div class="mt-4 text-center">
+              <button type="submit" class="btn btn-secondary">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ClientForm"
+}
+</script>
+
---
+++ client/src/components/ProductForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container-lg">
+      <div class="row justify-content-center my-5">
+        <div class="col-lg-6">
+          <form>
+            <label for="description" class="form-label"></label>
+            <div class="input-group">
+              <input type="text" id="description" class="form-control" placeholder="Nome do produto"/>
+            </div>
+            <label for="unities" class="form-label"></label>
+            <div class="input-group">
+              <input type="number" id="unities" class="form-control" placeholder="Unidades" />
+            </div>
+            <label for="p_value" class="form-label"></label>
+            <div class="input-group">
+              <input type="number" id="p_value" class="form-control" placeholder="Valor" />
+            </div>
+            <div class="mt-4 text-center">
+              <button type="submit" class="btn btn-secondary">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductForm"
+}
+</script>
