5cb5a8350d019a13d11b3eeb3be5242eda73c50a
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-12 11:27:15 -0300

    feat: create client dummy card and adding to the list

---
+++ client/src/components/ClientCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card m-1" style="width: 16.5rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item bg-success text-light">Jõao Carvalho</li>
+    <li class="list-group-item bg-success text-light">199.188.177-53</li>
+    <li class="list-group-item bg-success text-light">(99) 999776655</li>
+    <li class="list-group-item bg-success text-light">joao_c@gmail.com</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: "ClientCard"
+}
+</script>
+
--- client/src/components/ClientList.vue
+++ client/src/components/ClientList.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="text-center mt-5">
-        <p>Ainda não há clientes cadastrados</p>
+    <div class="text-center mt-5 mb-5 container d-flex flex-wrap">
+        <!-- <p>Ainda não há clientes cadastrados</p> -->
+        <ClientCard />
     </div>
 </template>
 
 <script>
+import ClientCard from "./ClientCard.vue"
+
 export default {
-    name: "ClientList"
+    name: "ClientList",
+    components: {
+        ClientCard
+    }
 }
 </script>
 
