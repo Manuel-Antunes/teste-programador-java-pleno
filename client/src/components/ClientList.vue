7c717d72443980f7633704c42da7bb0039b4aaee
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-12 11:12:47 -0300

    chore: create list component to get data from db

---
+++ client/src/components/ClientList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center mt-5">
+        <p>Ainda não há clientes cadastrados</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ClientList"
+}
+</script>
+
---
+++ client/src/components/ProductList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center mt-5">
+        <p>Ainda não há produtos cadastrados</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductList"
+}
+</script>
+
