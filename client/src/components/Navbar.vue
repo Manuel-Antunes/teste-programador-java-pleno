f238e92119bb14ab4cf8250a9ef6f9ee4edd05a6
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-12 02:38:55 -0300

    refactor: change nav-links by router-links

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="nav">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <router-link class="navbar-brand" to="/">
             <img class="fluid" alt="Vue logo" src="../assets/logo.png" width="48px"/>
             Companhia do seu Zé
-        </a>
+        </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav ">
-                <a class="nav-link" href="#">Clientes</a>
-                <a class="nav-link" href="#">Produtos</a>
-                <a class="nav-link" href="#">Pedidos</a>
+                <router-link class="nav-link" to="/clients">Clientes</router-link>
+                <router-link class="nav-link" to="/products">Produtos</router-link>
+                <router-link class="nav-link" to="/orders">Pedidos</router-link>
             </div>
         </div>
     </div>
