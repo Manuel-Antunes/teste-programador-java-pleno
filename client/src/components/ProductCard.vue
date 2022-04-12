862d05f0058d1361be098279896a93efbaccf06d
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-12 12:31:52 -0300

    feat: create productcard and adding it to the product list (dummy)

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card m-1" style="width: 15rem;">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-success text-light">CD Calcinha preta promocional</li>
+        <li class="list-group-item bg-success text-light">25 unidades</li>
+        <li class="list-group-item bg-success text-light">$ 55</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ProductCard"
+}
+</script>
+
--- client/src/components/ProductList.vue
+++ client/src/components/ProductList.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="text-center mt-5">
-        <p>Ainda não há produtos cadastrados</p>
+    <div class="text-center mt-3 container d-flex flex-wrap">
+        <!-- <p>Ainda não há produtos cadastrados</p> -->
+        <ProductCard />
     </div>
 </template>
 
 <script>
+import ProductCard from "./ProductCard.vue"
+
 export default {
-    name: "ProductList"
+    name: "ProductList",
+    components: {
+        ProductCard
+    }
 }
 </script>
 
