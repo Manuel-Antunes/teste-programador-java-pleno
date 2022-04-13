2fa8f3e573cb1ed2fb7ea3aaa22d9e34065e3243
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 20:08:10 -0300

    chore: get the logic of update data in db in this component

---
+++ client/src/views/UpdateClient.vue
@@ -0,0 +1,41 @@
+<template>
+    <ClientForm @onSubmit="onSubmit"/>
+</template>
+
+<script>
+import ClientForm from "../components/Clients/ClientForm.vue"
+import apolloClient from "../apollo/client"
+import { updateUser } from "../apollo/mutations/updateUser"
+
+export default {
+    name: "UpdateClient",
+    components: {
+        ClientForm
+    },
+    data() {
+        return {
+            user: {
+                name: "Manuel ChupaPikax",
+                cpf: "112301423-55",
+                phone: "(65) 99999999",
+                email: "manuelchupabem@email.com"
+            }
+        }
+    },
+    methods: {
+        async onSubmit() {
+            try {
+                const {data} = await apolloClient.mutate(updateUser(formData))
+            } 
+            catch(err) {
+                this.$swal({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: err.message,
+                })
+            }   
+        }
+    }
+}
+</script>
+
---
+++ client/src/views/UpdateProduct.vue
@@ -0,0 +1,30 @@
+<template>
+    <ProductForm @onSubmit="onSubmit"/>
+</template>
+
+<script>
+import ProductForm from "../components/Products/ProductForm.vue"
+import {updateProduct} from "../apollo/mutations/updateProduct"
+export default {
+    name: "UpdateProduct",
+    components: {
+        ProductForm
+    },
+    methods: {
+        onSubmit() {
+            try {
+                const { data } = await apolloClient.mutate(createProduct(formData))
+            } 
+            catch(err) {
+                this.$swal({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: err.message,
+                })
+            }
+        }
+    }
+
+}
+</script>
+
