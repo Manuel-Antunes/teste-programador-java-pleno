b830f6ba9b4ba911d58b6cb56649e30f30a6dbe9
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 14:12:45 -0300

    chore: add edit & remove buttons to product card

--- client/src/components/Products/ProductCard.vue
+++ client/src/components/Products/ProductCard.vue
@@ -4,6 +4,13 @@
         <li class="list-group-item bg-success text-light">CD Calcinha preta promocional</li>
         <li class="list-group-item bg-success text-light">25 unidades</li>
         <li class="list-group-item bg-success text-light">$ 55</li>
+        <li class="list-group-item bg-secondary text-light">
+          <div class="d-flex justify-content-around">
+            <button class="btn btn-light">Editar</button>
+            <button class="btn btn-danger">Excluir</button>
+          </div>
+        </li>
+
     </ul>
   </div>
 </template>
