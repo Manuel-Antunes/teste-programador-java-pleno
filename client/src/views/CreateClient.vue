04b2f03695e37dff8a07b1084bb85109a85d486f
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 00:57:52 -0300

    feat: add different routes to add products and clients

---
+++ client/src/views/CreateClient.vue
@@ -0,0 +1,22 @@
+<template>
+  <ClientForm @clientCreated="onClientCreated" />
+</template>
+
+<script>
+import ClientForm from "../components/Clients/ClientForm.vue"
+
+export default {
+    name: "CreateClient",
+    components: {
+        ClientForm
+    },
+    methods: {
+      onClientCreated() {
+        this.$router.push({
+          path: "/clients"
+        })
+      }
+    }
+}
+</script>
+
---
+++ client/src/views/CreateProduct.vue
@@ -0,0 +1,21 @@
+<template>
+  <ProductForm @productCreated="onProductCreated" />
+</template>
+
+<script>
+import ProductForm from "../components/Products/ProductForm.vue"
+export default {
+    name: "CreateProduct",
+    components: {
+        ProductForm
+    },
+    methods: {
+      onProductCreated() {
+        this.$router.push({
+          path: "/products"
+        })
+      }
+    }
+}
+</script>
+
