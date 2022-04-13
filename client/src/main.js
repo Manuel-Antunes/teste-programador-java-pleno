d15a366a851f199cd73d8a8aa610e236d288f3a9
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 00:52:48 -0300

    feat: add vuesweet2 to the main component

--- client/src/main.js
+++ client/src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(VueSweetalert2)
+app.mount("#app")
