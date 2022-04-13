9dccb97fb57acb6db1bfcd2c50ad488a8198f184
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 00:55:05 -0300

    chore: change ngrok link

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink } from '@apollo/client/core'
 import { InMemoryCache } from '@apollo/client/cache'
 
 const httpLink = new HttpLink({
-  uri: "http://0419-168-228-85-114.ngrok.io/graphql"
+  uri: "https://929b-168-228-85-114.ngrok.io/graphql"
 })
 
 const apolloClient = new ApolloClient({
