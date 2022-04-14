5e6d3528c3017dc17ab35d4a11ac8328e2bd795a
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-14 12:25:11 -0300

    fix: capitalize DeleteProduct mutation

--- client/src/apollo/mutations/deleteProduct.js
+++ client/src/apollo/mutations/deleteProduct.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client/core"
 
 export const deleteProduct = gql`
-    mutation deleteProduct($code: String!){
+    mutation DeleteProduct($code: String!){
         deleteProduct(code: $code)
   }  
 `
