d0f1b0b31327c400653672de9fcd4914e5dd3d3a
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-14 04:23:12 -0300

    feat: create feature to update products

--- client/src/views/UpdateProduct.vue
+++ client/src/views/UpdateProduct.vue
@@ -1,31 +1,66 @@
 <template>
-    <ProductForm @onSubmit="onSubmit"/>
+    <ProductForm @onSubmit="onSubmit" :data="product"/>
 </template>
 
 <script>
+import apolloClient from "../apollo/client"
 import ProductForm from "../components/Products/ProductForm.vue"
 import {updateProduct} from "../apollo/mutations/updateProduct"
+import { getProduct } from '../apollo/queries/getProduct'
 
 export default {
     name: "UpdateProduct",
     components: {
         ProductForm
     },
+    data() {
+        return {
+            product: {}
+        }
+    },
+    async mounted() {
+        try {
+            const code = this.$route.params.id
+            const { data } = await apolloClient.query({
+                query: getProduct,
+                variables: {
+                    code
+                }
+            })
+            this.product = data.product
+        } 
+        catch(err) {
+            this.$swal({
+                icon: 'error',
+                title: 'Oops...',
+                text: err.message,
+            })
+        }
+    },
     methods: {
         async onSubmit(formData) {
             try {
-                const { data } = await apolloClient.mutate({
+                console.log(formData)
+                const code = this.$route.params.id
+                await apolloClient.mutate({
                     mutation: updateProduct,
-                    variables: formData
+                    variables: {
+                        code,
+                        ...formData
+                    },
+                })
+                this.$router.push({
+                    path: `/products`
                 })
             } 
             catch(err) {
+                console.log(JSON.stringify(err))
                 this.$swal({
                     icon: 'error',
                     title: 'Oops...',
                     text: err.message,
                 })
-            }
+            }   
         }
     }
 
