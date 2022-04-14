2003c4f5d048d50ca1e2ab25100930616a0379f7
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-13 23:56:12 -0300

    chore: remove hardcoded data

--- client/src/components/Products/ProductCard.vue
+++ client/src/components/Products/ProductCard.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="card m-1" style="width: 15rem;">
     <ul class="list-group list-group-flush">
-        <li class="list-group-item bg-success text-light">CD Calcinha preta promocional</li>
-        <li class="list-group-item bg-success text-light">25 unidades</li>
-        <li class="list-group-item bg-success text-light">$ 55</li>
+        <li class="list-group-item bg-success text-light">{{name}}</li>
+        <li class="list-group-item bg-success text-light">{{desc}}</li>
+        <li class="list-group-item bg-success text-light">{{units}} unidades</li>
+        <li class="list-group-item bg-success text-light">R$ {{price}}</li>
         <li class="list-group-item bg-secondary text-light">
           <div class="d-flex justify-content-around">
             <button class="btn btn-light">Editar</button>
@@ -17,7 +18,26 @@
 
 <script>
 export default {
-    name: "ProductCard"
+    name: "ProductCard",
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      desc: {
+        type: String,
+        required: true
+      }, 
+      units: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      }
+
+    }
 }
 </script>
 
