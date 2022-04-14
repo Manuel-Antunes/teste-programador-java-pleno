09f3eb098ec9acf9083a881c960d3262a48e6908
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-14 06:29:01 -0300

    feat: add component to create new orders

---
+++ client/src/views/CreateOrder.vue
@@ -0,0 +1,42 @@
+<template>
+    <OrderForm @onSubmit="onSubmit"/>
+</template>
+
+<script>
+import OrderForm from "../components/Orders/OrderForm.vue"
+import apolloClient from "../apollo/client"
+import { createOrder } from "../apollo/mutations/createOrder"
+
+export default {
+    name: "CreateOrder",
+    components: {
+        OrderForm
+    },
+    methods: {
+        async onSubmit(formData) {
+            try {
+                const code = this.$route.params.id
+                await apolloClient.mutate({
+                    mutation: createOrder,
+                    variables: {
+                        code,
+                        product_ids: [],
+                        ...formData
+                    }
+                })
+                this.$router.push({
+                    path: `/clients/${code}/orders`
+                })
+            } 
+            catch(err) {
+                console.log(JSON.stringify(err))
+                this.$swal({
+                icon: 'error',
+                title: 'Oops...',
+                text: err.message,
+                })
+            }
+        }
+    }
+}
+</script>
