6129adc2c488caf36b0f3d3e0e68cabfd7000769
Pedro Valente <valentedev09@gmail.com>
2022-04-14 15:22:27 -0300

    a

--- client/server.js
+++ client/server.js
@@ -1,13 +1,16 @@
+const { createServer } = require('vite')
 const express = require('express')
 const path = require('path')
 
+const vite = createServer({ server: { middlewareMode: "ssr" } })
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+app.get('*', function (req, res) {
+    const alguma = (path.join(__dirname, 'dist', 'index.html'))
+    console.log(alguma);
+    res.sendFile(alguma)
 })
 
 app.listen(PORT, () => console.log(`Listening on :${PORT}`))
