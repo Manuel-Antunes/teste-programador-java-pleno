5a4b62d2a7d11a5c084495fee4761c80bab7c577
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-14 06:28:21 -0300

    chore: formating date and currency

--- client/src/views/ClientOrders.vue
+++ client/src/views/ClientOrders.vue
@@ -12,7 +12,10 @@
         <p v-if="!customer.orders || customer.orders.length == 0" class="text-center mt-5">Nenhum pedido feito por esse cliente</p>
         <ul class="list-group" v-else>
         <li v-for="(order) in customer.orders" :key="order.code" class="list-group-item">
-            <span>{{order.issue_date}}</span>
+            <div class="d-flex justify-content-between">
+                <span>Data de emissão: {{order.issue_date}}</span>
+                <span>Valor total: {{order.total_price.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}}</span>
+          </div>
             <table class="table">
                 <thead>
                     <tr>
@@ -22,20 +25,24 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <p v-if="order.products.length === 0" class="fw-bold mt-5">Não há produtos nesse pedido</p>
                      <tr v-for="(product) in order.products" :key="product.code">
                         <th scope="row">{{product.code}}</th>
                         <td>{{product.name}}</td>
-                        <td>{{product.price}}</td>
+                        <td>{{product.price.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}}</td>
                         </tr>
                 </tbody>
             </table>
             <div class="d-flex justify-content-end mt-3">
-                <router-link :to="`/clients/${customer.code}/orders/${order.id}/products/add`">
+                <router-link :to="`/clients/${customer.code}/orders/${order.number}/products/add`">
                     <button class="btn btn-secondary">Adicionar produtos</button>
                 </router-link>
             </div>
         </li>
     </ul>
+        <router-link :to="`/clients/${customer.code}/orders/add`">
+            <button class="btn btn-success w-100">Adicionar pedido</button>
+        </router-link>
     </div>
 </template>
 
