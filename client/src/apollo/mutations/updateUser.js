ed6d56757dfecea87979c1c2d7b5a4666a23143d
Marcelo Ferreira <flaviomff@gmail.com>
2022-04-14 02:19:02 -0300

    chore: add code param to the graphql

--- client/src/apollo/mutations/updateUser.js
+++ client/src/apollo/mutations/updateUser.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client/core"
 
 export const updateUser = gql`
-    mutation UpdateUser($name: String!, $email: String!, $cpf: String!, $phone_number: String!) {
-        updateUser(user_name: $name, email:$email, cpf: $cpf, phone_number: $phone) {
+    mutation UpdateUser($code: String!, $name: String, $email: String, $cpf: String, $phone: String) {
+        updateUser(code: $code, user_name: $name, email:$email, cpf: $cpf, phone_number: $phone) {
             user_name 
             email
             cpf
