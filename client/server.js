c30066341dc81128d05dd1957feffda38cf07038
Pedro Valente <valentedev09@gmail.com>
2022-04-14 15:23:40 -0300

    a

--- client/server.js
+++ client/server.js
@@ -1,16 +1,27 @@
-const { createServer } = require('vite')
-const express = require('express')
+const fs = require('fs')
 const path = require('path')
+const express = require('express')
+const { createServer: createViteServer } = require('vite')
 
-const vite = createServer({ server: { middlewareMode: "ssr" } })
-const app = express()
-const PORT = process.env.PORT || 3000
+async function createServer() {
+  const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('*', function (req, res) {
-    const alguma = (path.join(__dirname, 'dist', 'index.html'))
-    console.log(alguma);
-    res.sendFile(alguma)
-})
+  // Create Vite server in middleware mode. This disables Vite's own HTML
+  // serving logic and let the parent server take control.
+  //
+  // In middleware mode, if you want to use Vite's own HTML serving logic
+  // use `'html'` as the `middlewareMode` (ref https://vitejs.dev/config/#server-middlewaremode)
+  const vite = await createViteServer({
+    server: { middlewareMode: 'ssr' }
+  })
+  // use vite's connect instance as middleware
+  app.use(vite.middlewares)
 
-app.listen(PORT, () => console.log(`Listening on :${PORT}`))
+  app.use('*', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  })
+
+  app.listen(3000)
+}
+
+createServer()
